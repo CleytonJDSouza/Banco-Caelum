decb37f3e70862e58c99ca952d0a2300e2895dfd
Cleyton <cleytonjosedesouza@hotmail.com>
2023-11-30 12:49:20 -0300

    Código Banco Java 30/11/2023

--- fj11-contas/src/br/com/caelum/contas/main/NumerosOrdenados.java
+++ fj11-contas/src/br/com/caelum/contas/main/NumerosOrdenados.java
@@ -1,6 +1,30 @@
 package br.com.caelum.contas.main;
 
-import java.util.TreeSet;
+// USANDO O ARRAYLIST
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class NumerosOrdenados {
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> numeros = new ArrayList<>();
+		
+		for (int i = 1; i <= 1000; i++) {
+			numeros.add(i);
+		}
+		
+		Collections.sort(numeros, Collections.reverseOrder());
+		
+		for (Integer numero : numeros) {
+			System.out.println(numero);
+		}
+	}
+}
+
+// USANDO O TREESET
+
+/*import java.util.TreeSet;
 
 public class NumerosOrdenados {
 
@@ -15,4 +39,5 @@ public class NumerosOrdenados {
             System.out.println(numero);
         }
     }
-}
+}*/
+
