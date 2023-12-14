fe6378cb8e660f1ba96a888cceae774b1c61b295
Cleyton <cleytonjosedesouza@hotmail.com>
2023-12-14 16:46:31 -0300

    Código Banco Java 14/12/2023

--- fj11-contas/src/br/com/caelum/contas/modelo/ProduzMensagens.java
+++ fj11-contas/src/br/com/caelum/contas/modelo/ProduzMensagens.java
@@ -16,8 +16,10 @@ public class ProduzMensagens implements Runnable {
 	
 	@Override
 	public void run() {
-		for (int i = comeco; i < fim; i ++) {
-			mensagens.add("Mensagem " + i);
+		for (int i = comeco; i < fim; i++) {
+			synchronized (mensagens) {
+				mensagens.add("Mensagem " + i);
+			}
 		}
 	}
 
