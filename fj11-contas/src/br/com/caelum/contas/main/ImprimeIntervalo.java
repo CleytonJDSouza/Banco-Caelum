bdca0ac62edb558786721636d1be3c37ffaf7220
Cleyton <cleytonjosedesouza@hotmail.com>
2023-12-18 16:27:01 -0300

    Código Banco Java 18/12/2023

---
+++ fj11-contas/src/br/com/caelum/contas/main/ImprimeIntervalo.java
@@ -0,0 +1,20 @@
+package br.com.caelum.contas.main;
+
+public class ImprimeIntervalo {
+	
+	public static void main(String[] args) {
+		
+		//LOOP FOR
+		/*for(int i = 150; i <= 300; i++) {
+			System.out.println(i);
+		}*/
+		
+		//LOOP WHILE
+		int i = 150;
+		while (i <= 300) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
