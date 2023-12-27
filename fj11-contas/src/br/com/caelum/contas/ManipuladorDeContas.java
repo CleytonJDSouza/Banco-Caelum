d981a1ef9292c6a29ebfa0b2fe9c69ff4a30d160
Cleyton <cleytonjosedesouza@hotmail.com>
2023-12-27 15:34:30 -0300

    Cpodigo Banco Java 27/12/2023

---
+++ fj11-contas/src/br/com/caelum/contas/ManipuladorDeContas.java
@@ -0,0 +1,29 @@
+package br.com.caelum.contas;
+
+import br.com.caelum.contas.modelo.Conta;
+import br.com.caelum.contas.modelo.Evento;
+
+public class ManipuladorDeContas {
+	
+	private Conta conta;
+	
+	public void criaConta(Evento evento) {
+		this.conta = new Conta();
+		
+		this.conta.setTitular(evento.getTitular());
+		this.conta.setNumero(evento.getNumero());
+		this.conta.setAgencia(evento.getAgencia());
+	}
+	
+	public void deposita(Evento evento) {
+		
+		double valor = evento.getValorOperacao();
+		
+		if (valor > 0) {
+			this.conta.deposita(valor);
+			System.out.println("Depósito de R$" + valor + " realizado com sucesso.");
+		} else {
+			System.out.println("Valor do depósito deve ser maior que zero.");
+		}
+	}
+}
